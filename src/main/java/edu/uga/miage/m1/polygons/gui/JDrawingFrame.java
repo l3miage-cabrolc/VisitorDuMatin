0a9a6ef8e758794254e97c933e6edcc9652ab405
Camille <camille.cabrol1@etu.univ-grenoble-alpes.fr>
2022-10-03 16:44:14 +0200

    1st commit

---
+++ src/main/java/edu/uga/miage/m1/polygons/gui/JDrawingFrame.java
@@ -0,0 +1,268 @@
+package edu.uga.miage.m1.polygons.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+
+import edu.uga.miage.m1.polygons.gui.persistence.JSonVisitor;
+import edu.uga.miage.m1.polygons.gui.persistence.XMLVisitor;
+import edu.uga.miage.m1.polygons.gui.shapes.Circle;
+import edu.uga.miage.m1.polygons.gui.shapes.Square;
+import edu.uga.miage.m1.polygons.gui.shapes.Triangle;
+
+/**
+ * This class represents the main application class, which is a JFrame subclass
+ * that manages a toolbar of shapes and a drawing canvas.
+ *
+ * @author <a href="mailto:christophe.saint-marcel@univ-grenoble-alpes.fr">Christophe</a>
+ */
+public class JDrawingFrame extends JFrame implements MouseListener, MouseMotionListener {
+
+    private enum Shapes {
+
+        SQUARE, TRIANGLE, CIRCLE
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    private JToolBar mToolBar;
+
+    private Shapes mSelected;
+
+    private JPanel mPanel;
+
+    private JLabel mLabel;
+
+    private ActionListener m_reusableActionListener = new ShapeActionListener();
+
+    private ActionListener saveJson = new JsonSaveActionListener();
+    private ActionListener saveXML = new XMLSaveActionListener();
+
+    private JSonVisitor jsonVisitor = new JSonVisitor();
+
+   private XMLVisitor xmlVisitor = new XMLVisitor();
+
+    /**
+     * Tracks buttons to manage the background.
+     */
+    private Map<Shapes, JButton> mButtons = new HashMap<>();
+
+    /**
+     * Default constructor that populates the main window.
+     * @param frameName
+     */
+    public JDrawingFrame(String frameName) {
+        super(frameName);
+
+    
+        // Instantiates components
+        mToolBar = new JToolBar("Toolbar");
+        mPanel = new JPanel();
+        mPanel.setBackground(Color.WHITE);
+        mPanel.setLayout(null);
+        mPanel.setMinimumSize(new Dimension(400, 400));
+        mPanel.addMouseListener(this);
+        mPanel.addMouseMotionListener(this);
+        mLabel = new JLabel(" ", JLabel.LEFT);
+        // Fills the panel
+        setLayout(new BorderLayout());
+        add(mToolBar, BorderLayout.NORTH);
+        add(mPanel, BorderLayout.CENTER);
+        add(mLabel, BorderLayout.SOUTH);
+        // Add shapes in the menu
+
+        addSave();
+
+        addShape(Shapes.SQUARE, new ImageIcon(getClass().getResource("images/square.png")));
+        addShape(Shapes.TRIANGLE, new ImageIcon(getClass().getResource("images/triangle.png")));
+        addShape(Shapes.CIRCLE, new ImageIcon(getClass().getResource("images/circle.png")));
+        setPreferredSize(new Dimension(400, 400));
+    }
+
+
+
+    private void addSave(){
+        JButton b1 = new JButton("save to Json");
+        b1.addActionListener(saveJson);
+        mToolBar.add(b1);
+
+        JButton b2 = new JButton("save to XML");
+        b1.addActionListener(saveXML);
+        mToolBar.add(b2);
+    }
+
+    /**
+     * Injects an available <tt>SimpleShape</tt> into the drawing frame.
+     * @param name The name of the injected <tt>SimpleShape</tt>.
+     * @param icon The icon associated with the injected <tt>SimpleShape</tt>.
+     */
+    private void addShape(Shapes shape, ImageIcon icon) {
+        JButton button = new JButton(icon);
+        button.setBorderPainted(false);
+        mButtons.put(shape, button);
+        button.setActionCommand(shape.toString());
+        button.addActionListener(m_reusableActionListener);
+        if (mSelected == null) {
+            button.doClick();
+        }
+        mToolBar.add(button);
+        mToolBar.validate();
+        repaint();
+    }
+
+    /**
+     * 
+     *      * Implements method for the <tt>MouseListener</tt> interface to
+     * draw the selected shape into the drawing canvas.
+     * @param evt The associated mouse event.
+     */
+    public void mouseClicked(MouseEvent evt) {
+        if (mPanel.contains(evt.getX(), evt.getY())) {
+            Graphics2D g2 = (Graphics2D) mPanel.getGraphics();
+
+            switch(mSelected) {
+                case CIRCLE:
+                    Circle circle = new Circle(evt.getX(), evt.getY());
+                    circle.draw(g2);
+                    jsonVisitor.visit(circle);
+                    xmlVisitor.visit(circle);
+                    break;
+                case TRIANGLE:
+                    Triangle triangle = new Triangle(evt.getX(), evt.getY());
+                    triangle.draw(g2);
+                    jsonVisitor.visit(triangle);
+                    xmlVisitor.visit(triangle);
+                    break;
+                case SQUARE:
+                    Square square = new Square(evt.getX(), evt.getY());
+                    square.draw(g2);
+                    jsonVisitor.visit(square);
+                    xmlVisitor.visit(square);
+                    break;
+                default:
+                    System.out.println("No shape named " + mSelected);
+            }
+        
+            
+
+        }
+    }
+
+    /**
+     * Implements an empty method for the <tt>MouseListener</tt> interface.
+     * @param evt The associated mouse event.
+     */
+    public void mouseEntered(MouseEvent evt) {
+    }
+
+    /**
+     * Implements an empty method for the <tt>MouseListener</tt> interface.
+     * @param evt The associated mouse event.
+     */
+    public void mouseExited(MouseEvent evt) {
+        mLabel.setText(" ");
+        mLabel.repaint();
+    }
+
+    /**
+     * Implements method for the <tt>MouseListener</tt> interface to initiate
+     * shape dragging.
+     * @param evt The associated mouse event.
+     */
+    public void mousePressed(MouseEvent evt) {
+    }
+
+    /**
+     * Implements method for the <tt>MouseListener</tt> interface to complete
+     * shape dragging.
+     * @param evt The associated mouse event.
+     */
+    public void mouseReleased(MouseEvent evt) {
+    }
+
+    /**
+     * Implements method for the <tt>MouseMotionListener</tt> interface to
+     * move a dragged shape.
+     * @param evt The associated mouse event.
+     */
+    public void mouseDragged(MouseEvent evt) {
+    }
+
+    /**
+     * Implements an empty method for the <tt>MouseMotionListener</tt>
+     * interface.
+     * @param evt The associated mouse event.
+     */
+    public void mouseMoved(MouseEvent evt) {
+        modifyLabel(evt);
+    }
+
+    private void modifyLabel(MouseEvent evt) {
+        mLabel.setText("(" + evt.getX() + "," + evt.getY() + ")");
+    }
+
+    /**
+     * Simple action listener for shape tool bar buttons that sets
+     * the drawing frame's currently selected shape when receiving
+     * an action event.
+     */
+    private class ShapeActionListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent evt) {
+            // Itère sur tous les boutons
+            Iterator<Shapes> keys = mButtons.keySet().iterator();
+            while (keys.hasNext()) {
+                Shapes shape = keys.next();
+                JButton btn = mButtons.get(shape);
+                if (evt.getActionCommand().equals(shape.toString())) {
+                    btn.setBorderPainted(true);
+                    mSelected = shape;
+                } else {
+                    btn.setBorderPainted(false);
+                }
+                btn.repaint();
+            }
+        }
+    }
+
+
+    private class JsonSaveActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                try {
+                    jsonVisitor.save();
+                } catch (IOException e1) {
+                }
+        }
+    }
+
+    private class XMLSaveActionListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                xmlVisitor.save();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+
+    
+}
